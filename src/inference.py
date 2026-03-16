ae0d6c4c14c033fbc640d77a82c010eef7d0af42
ducholmes <84381443+ducholmes@users.noreply.github.com>
2026-03-16 07:59:23 +0700

    Fix

--- src/inference.py
+++ src/inference.py
@@ -52,9 +52,9 @@ def predict_and_draw(image_path, checkpoint_path):
     
     plt.title(f"Face Landmark Detection - {image_path.split('/')[-1]}")
     plt.axis('on')
-    plt.savefig('images/Nigga_plot.png')
+    plt.savefig('images/Niggaa_plot.png')
 
-TEST_IMAGE_PATH = "images/Nigga.jpg" 
+TEST_IMAGE_PATH = "images/Niggaa.jpg" 
 CKPT_PATH = "logs/train/last.ckpt"
 
 def decode_heatmaps(self, heatmaps):

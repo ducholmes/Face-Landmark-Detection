0f6ce448baa71fb7c023dd9c3bea6dfdd0423fb6
ducholmes <84381443+ducholmes@users.noreply.github.com>
2026-03-17 09:48:29 +0700

    Fix

--- src/data/components/wflw_dataset.py
+++ src/data/components/wflw_dataset.py
@@ -27,7 +27,7 @@ class WFLWDataset(Dataset):
             for line in lines:
                 parts = line.strip().split()
 
-                landmark = np.array(parts[:196], dtype=np.float32).reshape(-1, 2)
+                landmark = np.array(parts[0:196], dtype=np.float32).reshape(-1, 2)
                 landmark_name = parts[-1]
 
                 self.landmarks.append((landmark, landmark_name))

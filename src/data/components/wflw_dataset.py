6c70de44f07922edc56e31f4fbeef55fb7a0e8f7
ducholmes <84381443+ducholmes@users.noreply.github.com>
2026-03-17 08:50:30 +0700

    Fix

---
+++ src/data/components/wflw_dataset.py
@@ -0,0 +1,95 @@
+import os
+from pathlib import Path
+
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+from PIL import Image
+
+class WFLWDataset(Dataset):
+    def __init__(
+        self,
+        img_dir,
+        landmark_path,
+        transform = None,
+        img_size=(256, 256),    
+        heatmap_size=(64, 64),  
+        sigma=2.0,  
+        num_keypoints=98
+    ):
+        self.img_dir = Path(img_dir)
+
+        self.landmarks = np.array([])
+
+        with open(landmark_path, 'r') as f:
+            lines = f.readline()
+
+            for line in lines:
+                parts = line.strip().split()
+
+                landmark = np.array(parts[:196], dtype=np.float32).reshape(-1, 2)
+                landmark_name = parts[-1]
+
+                self.landmarks.append((landmark, landmark_name))
+
+        self.transform = transform
+        self.width, self.height = img_size
+        self.heatmap_width, self.heatmap_height = heatmap_size
+        self.sigma = sigma
+        self.num_keypoints = num_keypoints
+
+        x_grid = np.arange(0, self.heatmap_width, 1, dtype=np.float32)
+        y_grid = np.arange(0, self.heatmap_height, 1, dtype=np.float32)
+        self.xx, self.yy = np.meshgrid(x_grid, y_grid)
+
+
+    def _generate_heatmap(self, keypoints, mask):
+        heatmaps = np.zeros(
+            (self.num_keypoints, self.heatmap_height, self.heatmap_width), 
+            dtype=np.float32
+        )
+
+        for i in range(self.num_keypoints):
+            if not mask[i]:
+                continue 
+
+            x_hm = keypoints[i, 0] * (self.heatmap_width / self.width)
+            y_hm = keypoints[i, 1] * (self.heatmap_height / self.height)
+
+            heatmaps[i] = np.exp(
+                -((self.xx - x_hm)**2 + (self.yy - y_hm)**2) / (2 * self.sigma**2)
+            )
+
+        return heatmaps
+    
+    def __len__(self):
+        return len(self.landmarks)
+    
+    def __getitem__(self, index):
+        landmark, landmark_name = self.landmarks[index]
+
+        img_path = os.path.join(self.img_dir, landmark_name)
+
+        img = Image.open(img_path)
+
+        if self.transform:
+            transformed = self.transform(img, landmark)
+
+            img = transformed['image']
+            landmark = np.array(transformed['keypoints'])
+
+        mask = (landmark[:, 0] > 0) & (landmark[:, 1] > 0)
+
+        heatmap = self._generate_heatmap(landmark, mask)
+
+        keypoint_rel = np.copy(landmark)
+        keypoint_rel[:, 0] = keypoint_rel[:, 0] / self.width
+        keypoint_rel[:, 1] = keypoint_rel[:, 1] / self.height
+
+        return {
+            'image': img,                         
+            'heatmap': torch.tensor(heatmap),          
+            'keypoint_rel': torch.tensor(keypoint_rel),
+            'mask': torch.tensor(mask)
+        }
+
---
+++ src/data/wflw_datamodule.py
@@ -0,0 +1,124 @@
+import random
+from typing import Any, Tuple, Dict
+from lightning import LightningDataModule
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import DataLoader, Subset, random_split
+from torchvision import transforms
+import albumentations as A
+from src.data.components.wflw_dataset import WFLWDataset
+
+class WFLWDataModule(LightningDataModule):
+    def __init__(
+        self,
+        data_dir: str = '/data',
+        train_val_test_split: Tuple[int, int, int]=[0.8, 0.2, 0],
+        batch_size: int = 64,
+        num_workers: int = 3,
+        pin_memory: bool = False
+    ) -> None:
+        super().__init__()
+
+        self.save_hyperparameters(logger=False)
+
+        self.img_dir = data_dir + "/WFLW_images/WFLW_images"
+        self.train_keypoint_path = data_dir + "/WFLW_annotations/WFLW_annotations/list_98pt_rect_attr_train_test/list_98pt_rect_attr_train.txt"
+        self.test_keypoint_path = data_dir + "/WFLW_annotations/WFLW_annotations/list_98pt_rect_attr_train_test/list_98pt_rect_attr_test.txt"
+
+        self.train_transform = A.Compose([
+            A.LongestMaxSize(max_size=256),
+            A.PadIfNeeded(
+                min_height=256,
+                min_width=256,
+                border_mode=0,
+                value=(0,0,0),
+            ),
+            A.Normalize(),
+            A.ToTensorV2()
+        ], keypoint_params=A.KeypointParams(format='xy', remove_invisible=False))
+
+        self.test_transform = A.Compose([
+            A.LongestMaxSize(max_size=256),
+            A.PadIfNeeded(
+                min_height=256,
+                min_width=256,
+                border_mode=0,
+                value=(0,0,0),
+            ),
+            A.Normalize(),
+            A.ToTensorV2()
+        ], keypoint_params=A.KeypointParams(format='xy', remove_invisible=False))
+
+        self.data_train = None
+        self.data_test = None
+        self.data_val = None
+
+        self.batch_size_per_device = None
+
+    def train_dataloader(self) -> DataLoader[Any]:
+        return DataLoader(
+            dataset=self.data_train,
+            batch_size=self.batch_size_per_device,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=True,
+        )
+
+    def val_dataloader(self) -> DataLoader[Any]:
+        return DataLoader(
+            dataset=self.data_val,
+            batch_size=self.batch_size_per_device,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+
+    def test_dataloader(self) -> DataLoader[Any]:
+        return DataLoader(
+            dataset=self.data_test,
+            batch_size=self.batch_size_per_device,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+    
+    def setup(self, stage=None):
+        if self.trainer is not None:
+            if self.hparams.batch_size % self.trainer.world_size != 0:
+                raise RuntimeError(
+                    f"Batch size ({self.hparams.batch_size}) is not divisible by the number of devices ({self.trainer.world_size})."
+                )
+            self.batch_size_per_device = self.hparams.batch_size // self.trainer.world_size
+
+        if not self.data_train and not self.data_val and not self.data_test:
+            
+
+            if stage=='fit' or stage == None:
+                train_base = WFLWDataset(
+                    img_dir = self.img_dir,
+                    landmark_path=self.train_keypoint_path,
+                    transform=self.train_transform
+                )
+
+                self.data_train = train_base
+
+                val_base = WFLWDataset(
+                    img_dir=self.img_dir,
+                    landmark_path=self.test_keypoint_path,
+                    transform=self.test_transform
+                )
+
+                self.data_val = val_base
+
+            if stage=='test':
+                test_base = WFLWDataset(
+                    img_dir=self.img_dir,
+                    landmark_path=self.test_keypoint_path,
+                    transform=self.test_transform
+                )
+
+                self.data_test = test_base
+
+if __name__ == "__main__":
+    _ = KeypointDataModule()
